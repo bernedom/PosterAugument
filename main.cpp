71370caa2fdf65cb4d351fa3077f33a3617d9c79
Dominik Berner <dominik.berner@bbv.ch>
2017-06-12 14:41:52 +0200

    added comments and removed unused include to QtCoreApplication

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-#include <QCoreApplication>
-
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
@@ -7,7 +5,10 @@
 
 int main(int, char **) {
 
-  cv::VideoCapture stream(0);
+  cv::VideoCapture stream(0); // no const, because read() is not const
+
+  const auto source_image =
+      cv::imread("C:\\Code\\PosterAugument\\Assets\\JollyRoger.jpg");
 
   if (!stream.isOpened()) {
     std::cerr << "Could not open camera\n";
@@ -16,9 +17,11 @@ int main(int, char **) {
 
   while (true) {
     cv::Mat frame;
-    stream.read(frame);
+    stream.read(frame); // slurp a single frame from the webcam
 
-    cv::imshow("Cam output", frame);
+    cv::imshow("Cam output", frame); // put the image on screen
+
+    // wait for 30ms for a keypress and exit if any detected
     auto killer_key = cv::waitKey(30);
     if (killer_key >= 0 && killer_key < 255) {
       std::cout << "Killer key " << killer_key << std::endl;
