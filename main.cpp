8f91238e40b308e7a70546a8af5fb6202b4334df
Dominik Berner <dominik.berner@bbv.ch>
2017-06-12 16:31:07 +0200

    Doing SURF keypoint detection, but not using it

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <opencv2/core/core.hpp>
+#include <opencv2/features2d/features2d.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/xfeatures2d/nonfree.hpp >
 
 #include <iostream>
+#include <vector>
 
 int main(int, char **) {
 
@@ -10,14 +13,26 @@ int main(int, char **) {
   const auto source_image =
       cv::imread("C:\\Code\\PosterAugument\\Assets\\JollyRoger.jpg");
 
+  const auto min_hessian = 400;
+  cv::Ptr<cv::xfeatures2d::SURF> detector =
+      cv::xfeatures2d::SURF::create(min_hessian);
+
+  std::vector<cv::KeyPoint> key_points_source;
+  std::vector<cv::KeyPoint> key_points_input;
+
+  detector->detect(source_image, key_points_source);
+
   if (!stream.isOpened()) {
     std::cerr << "Could not open camera\n";
     return -1;
   }
 
   while (true) {
+    key_points_input.clear();
+
     cv::Mat frame;
     stream.read(frame); // slurp a single frame from the webcam
+    detector->detect(frame, key_points_input);
 
     cv::imshow("Cam output", frame); // put the image on screen
 
