00729f6820fd1665c7aba38fbc41ef4f6643f7c9
Dominik Berner <dominik.berner@bbv.ch>
2017-06-12 14:02:07 +0200

    Main does no longer use Qt-MainWindow

--- main.cpp
+++ main.cpp
@@ -3,26 +3,13 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-#include <QtWidgets/QMainWindow>
-
-class MW : public QMainWindow
-{
-public:
-    MW(QWidget* parent) : QMainWindow(parent)
-    {
-        cv::Mat image = cv::imread("C:\\Code\\PosterAugument\\Assets\\JollyRoger.jpg", 1);
-        cv::namedWindow("MyView");
-        cv::imshow("MyView", image);
-    }
-};
-
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    auto mw = new MW(nullptr);
-    mw->show();
-
+    cv::Mat image = cv::imread("C:\\Code\\PosterAugument\\Assets\\JollyRoger.jpg");
+    cv::namedWindow("MyView");
+    cv::imshow("MyView", image);
 
     return a.exec();
 }
