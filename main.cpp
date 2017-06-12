7060f9484bcda27b2625d8978bfad7eeb2d19c99
Dominik Berner <dominik.berner@bbv.ch>
2017-06-12 10:31:44 +0200

    simple mainwindow to execute cv code within

--- main.cpp
+++ main.cpp
@@ -3,13 +3,26 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
+#include <QtWidgets/QMainWindow>
+
+class MW : public QMainWindow
+{
+public:
+    MW(QWidget* parent) : QMainWindow(parent)
+    {
+        cv::Mat image = cv::imread("C:\\Code\\PosterAugument\\Assets\\JollyRoger.jpg", 1);
+        cv::namedWindow("MyView");
+        cv::imshow("MyView", image);
+    }
+};
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    cv::Mat image = cv::imread("C:\\Code\\PosterAugument\\Assets\\JollyRoger.jpg");
-    cv::namedWindow("MyView");
-    cv::imshow("MyView", image);
+    auto mw = new MW(nullptr);
+    mw->show();
+
 
     return a.exec();
 }
